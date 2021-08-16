24a7230aaa5d127d59f9dcf601d58c5c73696eef
Lacour Vincent <lacour.vincent.app@gmail.com>
2021-08-17 00:23:57 +0200

    Implement concurrency in solver

--- src/core/solver.py
+++ src/core/solver.py
@@ -1,16 +1,37 @@
-from itertools import combinations
+from itertools import permutations
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
 
 
 class Solver:
+
     def __init__(self, pattern, cards):
         self.pattern = pattern
         self.cards = cards
 
+    def is_permutation_valid(self, permutation):
+        pattern = self.pattern(*permutation)
+        if (pattern.is_pattern_valid()):
+            return permutation
+        return None
+
     def solve(self):
-        all_combinations = list([c for c in combinations(self.cards, self.pattern.TOTAL_CARDS)])
-        solutions = []
-        for combination in all_combinations:
-            pattern = self.pattern(*combination)
-            if (pattern.is_pattern_valid()):
-                solutions.append([*combination])
-        return solutions
+        all_permutations = self.get_all_permutations()
+        print(len(all_permutations))
+        results = []
+        with ThreadPoolExecutor(max_workers=None) as executor:
+            futures = {executor.submit(self.is_permutation_valid, perm): perm for perm in all_permutations}
+            for future in as_completed(futures):
+                results.append(future.result())
+        return [r for r in results if r is not None]
+
+    def get_all_permutations(self):
+        all_permutations = []
+        N = self.pattern.TOTAL_CARDS
+        for i, card in enumerate(self.cards):
+            if (i < len(self.cards) - N):
+                k = i // N
+                j = (k + 1) * N
+                omega = [card, *self.cards[j:]]
+                all_permutations.append([list(p) for p in permutations(omega, N)])
+        return [card for cards in all_permutations for card in cards]
